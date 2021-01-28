e91f818068722f36c305222b25b8bba63f59cda6
Shane Austin <shane.p.austin@gmail.com>
2021-01-28 13:07:16 +0000

    code

--- code/week02/bmi.py
+++ code/week02/bmi.py
@@ -3,6 +3,7 @@
 
 weight = int(input("Enter Weight (Kg): "))      #Request Weight
 height = int(input("Enter Height (CM): "))      #Request Height
+
 BMI = ((weight)/((height/100)*(height/100)))    #Calculate BMI and convert to metres
 
 print ('BMI: {:.1f}' .format(BMI))              #Print BMI to 1 decimal place    
