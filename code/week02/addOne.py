0e3cce4579b03b553f5be42ab42ebfd6dc3cd27f
Shane Austin <shane.p.austin@gmail.com>
2021-01-28 15:10:28 +0000

    Tidying Code

--- code/week02/addOne.py
+++ code/week02/addOne.py
@@ -1,7 +1,7 @@
 #Program that asks for a number and adds 1
 #Author: Shane Austin
 
-number = int(input("Enter you Number: ")) #Requests number and converts to an integer
-newNumber = number + 1 #Takes inputted number and adds 1
+number = int(input("Enter you Number: "))               #Requests number and converts to an integer
+newNumber = number + 1                                  #Takes inputted number and adds 1
 
-print ('{} plus 1 is {}'.format(number, newNumber)) #Displays response
+print ('{} plus 1 is {}'.format(number, newNumber))     #Displays response
--- code/week02/nameAndage.py
+++ code/week02/nameAndage.py
@@ -1,6 +1,6 @@
 #Program that asks for a Name and Age
 #Author: Shane Austin
 
-name = input("Enter your Name: ") #Requests name
-age = input("Enter your Age: ") #Requests age
-print ('Hello {}. \t Your age is {}.'.format(name,age)) #Displays response
+name = input("Enter your Name: ")                           #Requests name
+age = input("Enter your Age: ")                             #Requests age
+print ('Hello {}. \t Your age is {}.'.format(name,age))     #Displays response
