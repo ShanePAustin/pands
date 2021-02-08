543d20bd688451d33452ddc68b5862db75de0a1b
Shane Austin <shane.p.austin@gmail.com>
2021-02-08 15:17:00 +0000

    testing commit

--- code/week03/Lab3.2/convert.py
+++ code/week03/Lab3.2/convert.py
@@ -2,7 +2,9 @@
 #A program that takes a number and convets to cents
 #Author: Shane Austin
 
-num1 = float(input("Please enter an amount:- "))
+num1 = float(input("Please enter an amount: "))
 cent = round(num1 * 100)
+absoluteValue = abs(cent)
 
-print("That amount in cent is: {}".format(cent))
+
+print("That amount in cent is: {}" .format(absoluteValue))
